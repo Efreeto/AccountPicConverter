55b439c1cbcec791e37445d4d5c68d1b753c3cb1
Woo Kim <v-woki@microsoft.com>
2016-08-03 16:08:39 -0700

    Initial commit 2

---
+++ AccountPicConverter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace accountpicture_ms
+{
+    class AccountPicConverter
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide the .accountpicture-ms file path.");
+                return 1;
+            }
+
+            string filename = Path.GetFileNameWithoutExtension(args[0]);
+            Bitmap image96 = GetImage96(args[0]);
+            image96.Save(filename + "-96.bmp");
+            Bitmap image448 = GetImage448(args[0]);
+            image448.Save(filename + "-448.bmp");
+
+            Console.WriteLine("Extracted images successfully");
+            return 0;
+        }
+
+        public static Bitmap GetImage96(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            long position = Seek(fs, "JFIF", 0);
+            byte[] b = new byte[Convert.ToInt32(fs.Length)];
+            fs.Seek(position - 6, SeekOrigin.Begin);
+            fs.Read(b, 0, b.Length);
+            fs.Close();
+            fs.Dispose();
+            return GetBitmapImage(b);
+        }
+
+        public static Bitmap GetImage448(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            long position = Seek(fs, "JFIF", 100);
+            byte[] b = new byte[Convert.ToInt32(fs.Length)];
+            fs.Seek(position - 6, SeekOrigin.Begin);
+            fs.Read(b, 0, b.Length);
+            fs.Close();
+            fs.Dispose();
+            return GetBitmapImage(b);
+        }
+
+        public static Bitmap GetBitmapImage(byte[] imageBytes)
+        {
+            //var bitmapImage = new Bitmap();
+            //bitmapImage.BeginInit();
+            //bitmapImage.StreamSource = new MemoryStream(imageBytes);
+            //bitmapImage.EndInit();
+            //return bitmapImage;
+            var ms = new MemoryStream(imageBytes);
+            var bitmapImage = new Bitmap(ms);
+            return bitmapImage;
+        }
+
+        public static long Seek(System.IO.FileStream fs, string searchString, int startIndex)
+        {
+            char[] search = searchString.ToCharArray();
+            long result = -1, position = 0, stored = startIndex,
+            begin = fs.Position;
+            int c;
+            while ((c = fs.ReadByte()) != -1)
+            {
+                if ((char)c == search[position])
+                {
+                    if (stored == -1 && position > 0 && (char)c == search[0])
+                    {
+                        stored = fs.Position;
+                    }
+                    if (position + 1 == search.Length)
+                    {
+                        result = fs.Position - search.Length;
+                        fs.Position = result;
+                        break;
+                    }
+                    position++;
+                }
+                else if (stored > -1)
+                {
+                    fs.Position = stored + 1;
+                    position = 1;
+                    stored = -1;
+                }
+                else
+                {
+                    position = 0;
+                }
+            }
+
+            if (result == -1)
+            {
+                fs.Position = begin;
+            }
+            return result;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("accountpicture-ms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("accountpicture-ms")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c8d7f49c-7896-4d34-bd9f-c2226631654e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
